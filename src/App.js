ee6632a1209bae9cffc14c812cc8199e5689ce7e
ApelMahmud65 <apelmahmud.kpi@gmail.com>
2020-07-02 13:06:33 +0800

    added bootstrap with react-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+/*===============================================
+                  Bootstrap
+================================================*/
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Container>
+				<h1>This is Form</h1>
+				<Form.Group className="col-6">
+					<Form.Control type="email" placeholder="Type your email" />
+					<Form.Text className="text-muted">
+						We will never share your email
+					</Form.Text>
+				</Form.Group>
+			</Container>
+		</div>
+	);
 }
 
 export default App;
